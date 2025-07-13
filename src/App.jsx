271b3a1736cf051f2c7798948d6b42b4e6f36c39
fayarsh <faydarsh2000@gmail.com>
2025-07-13 12:40:05 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Welcome to Nutritracker</h1>
+      <h1 className='text-center'>Welcome to Nutritracker</h1>
       <p>Your personal nutrition tracking app</p>
     </div>
   )
